e2610c6471f2a2478255c2fc1f6fe1010c2deb59
manishde1234 <91026206+manishde1234@users.noreply.github.com>
2025-01-17 00:25:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (40.55%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
+
+    bool checkBit(int &x, int bit){
+        return x &(1 << bit);
+    }
+    bool setBit(int &x, int bit){
+        return x |= (1 << bit);
+    }
+    bool unsetBit(int &x, int bit){
+        return x &= ~(1 << bit);
+    }
     int minimizeXor(int num1, int num2) {
-        int a = __builtin_popcount(num1);
-        int b = __builtin_popcount(num2);
-        int res = num1;
-        for (int i = 0; i < 32; ++i) {
-            if (a > b && (1 << i) & num1) {
-                res ^= 1 << i;
-                --a;
-            }
-            if (a < b && !((1 << i) & num1)) {
-                res ^= 1 << i;
-                ++a;
+        int x = num1;
+        int requiredBit = __builtin_popcount(num2);
+        int currentBit = __builtin_popcount(num1);
+        int bit = 0;//start from least sig bit
+
+        if(requiredBit < currentBit){
+            while(requiredBit < currentBit){
+                if( checkBit(x,bit)){
+                    //bit is not set
+                    unsetBit(x,bit);
+                    currentBit--;
+                }
+                
+                    
+                    bit++;
+                
             }
         }
-        return res;
+        else if(currentBit < requiredBit){
+            while(currentBit < requiredBit){
+                if(!checkBit(x,bit)){
+                    //unset it 
+                    setBit(x,bit);
+                    currentBit++;
+                }
+                bit++;
+            }
+        }
+
+        return x;
     }
 };
