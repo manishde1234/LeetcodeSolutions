c2afd1f3e843aa3e0e977040bcac23d1a5f8ec2a
manishde1234 <91026206+manishde1234@users.noreply.github.com>
2025-02-25 20:53:28 +0530

    Time: 3 ms (35.91%) | Memory: 28.2 MB (55.34%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,26 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
-        int index = 0;
-        return construct(pre, post, index, 0, pre.size() - 1);
-    }
-    
-private:
-    TreeNode* construct(vector<int>& pre, vector<int>& post, int& index, int l, int h) {
-        if (index >= pre.size() || l > h) 
-            return nullptr;
-
-        TreeNode* root = new TreeNode(pre[index++]);
-        if (l == h) return root;
-
-        int i = l;
-        while (i <= h && post[i] != pre[index]) i++;
-
-        if (i <= h) {
-            root->left = construct(pre, post, index, l, i);
-            root->right = construct(pre, post, index, i + 1, h - 1);
+    TreeNode* solve(int preStart, int postStart, int preend, vector<int>& preorder, vector<int>& postorder, int n){
+        if(preStart > preend){
+            return NULL;
+        }
+       
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        if(preStart == preend){
+            return root;
         }
 
+        int nextNode = preorder[preStart+1];
+        int j = postStart;
+        while(postorder[j] != nextNode){
+            j++;
+        }
+        int num = j-postStart+1; 
+        root -> left = solve(preStart+1,postStart,preStart+num,preorder,postorder,n);
+        root -> right = solve(preStart+num+1,j+1,preend,preorder,postorder,n);
         return root;
+    }
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        int n = preorder.size();
+        return solve(0,0,n-1,preorder,postorder,n);
     }
 };
