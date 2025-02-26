71495998f45a7ccfa929b171d6cc1933cf73004a
manishde1234 <91026206+manishde1234@users.noreply.github.com>
2025-02-26 13:57:03 +0530

    Time: 83 ms (21.71%) | Memory: 356.1 MB (19.23%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,44 +1,60 @@
 class Solution {
 public:
+typedef long long ll;
+    bool solve(vector<int>& nums, vector<vector<int>>& queries, int mid, int &result){
+        int m = queries.size();
+        int n = nums.size();
+        vector<int>diff(n+1,0);
+        vector<int>prefixSum(n,0);
+        
+        for(int i = 0; i <= mid; i++){
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int val = queries[i][2];
+
+            diff[l] -= val;
+            diff[r+1] += val;
+        }
+
+        //calculate the prefixsum
+        prefixSum[0] = diff[0];
+        for(int i = 1; i < n; i++){
+            prefixSum[i] = prefixSum[i-1] + diff[i];
+        }
+
+        int zeroCount = 0;
+        for(int i = 0; i < n; i++){
+            if(abs(prefixSum[i]) >= nums[i]){
+                zeroCount++;
+            }
+        }
+        if(zeroCount == n){
+            result = min(result, mid);
+        }
+        return zeroCount == n;
+    }
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
         int m = queries.size();
-        auto check = [&](int mid){
-            vector<int> diff(n + 1, 0);
-            for(int i = 0; i <= mid; ++i){
-                vector<int> q = queries[i];
-                // cout << q[0] << " " << q[1] << " " << q[2] << " |";
-                diff[q[0]] -= q[2];
-                diff[q[1] + 1] += q[2]; 
-            }
-            for(int i = 1; i < n; ++i) diff[i] += diff[i - 1];
 
-            int zeroCount = 0;
-
-            for(int i = 0; i < n; ++i){
-                if(abs(diff[i]) >= nums[i]) zeroCount++;
-            }
-            return zeroCount == nums.size();
-
-        };
-        long long int sum = accumulate(nums.begin(), nums.end(), 0LL);
+        ll sum = accumulate(nums.begin(),nums.end(),0LL);
         if(sum == 0){
             return 0;
         }
-        int l = 0, r = m - 1;
-        int ans = INT_MAX;
-        while(l <= r){
-            int mid = l + (r - l)/2;
 
-            if(check(mid)){
-                // cout << mid << " ";
-                ans = min(ans, mid);
-                r = mid - 1;
+        int l = 0, r = m-1;
+        int result = INT_MAX;
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            bool flag = solve(nums, queries,mid,result);
+            if(flag){
+                r = mid-1;
             }
             else{
-                l = mid + 1;
+                l = mid+1;
             }
         }
-        return ans == INT_MAX ? -1 : ans + 1;
+
+        return result == INT_MAX ? -1: result+1;
     }
 };
