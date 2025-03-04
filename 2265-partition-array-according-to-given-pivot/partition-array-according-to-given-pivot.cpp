b33221c5cf014a5d0f0bfe0660ed264e42a443d9
manishde1234 <91026206+manishde1234@users.noreply.github.com>
2025-03-04 09:46:25 +0530

    Time: 14 ms (15.90%) | Memory: 127.7 MB (77.38%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> result(nums.size(), 0);
-        int left = 0, right = nums.size() - 1;
-        
-        for (int i = 0, j = nums.size() - 1; i < nums.size(); ++i, --j) {
-            if (nums[i] < pivot) {
-                result[left] = nums[i];
-                left++;
+        int n = nums.size();
+        vector<int>result(n);
+
+        int less = 0, equal = 0, more = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == pivot){
+                equal++;
             }
-            
-            if (nums[j] > pivot) {
-                result[right] = nums[j];
-                right--;
+            else if(nums[i] < pivot){
+                less++;
+            }
+            else{
+                more++;
+            }
+        }      
+
+        int i = 0, j = less, k = less + equal;
+
+        for(int m = 0; m < n; m++){
+            if(nums[m] < pivot){
+                result[i] = nums[m];
+                i++;
+            }
+            else if(nums[m] == pivot){
+                result[j] = nums[m];
+                j++;
+            }
+            else{
+                result[k] = nums[m];
+                k++;
             }
         }
-        
-        while (left <= right) {
-            result[left] = pivot;
-            left++;
-        }
-        
+
         return result;
     }
 };
