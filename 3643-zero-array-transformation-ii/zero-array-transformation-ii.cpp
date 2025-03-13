671231264c96d7dd3d58c36c86c6fec785d36650
manishde1234 <91026206+manishde1234@users.noreply.github.com>
2025-03-13 16:07:19 +0530

    Time: 59 ms (59.42%) | Memory: 338.8 MB (55.65%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,60 +1,68 @@
 class Solution {
 public:
-typedef long long ll;
-    bool solve(vector<int>& nums, vector<vector<int>>& queries, int mid, int &result){
-        int m = queries.size();
-        int n = nums.size();
-        vector<int>diff(n+1,0);
-        vector<int>prefixSum(n,0);
+    bool checkZero(vector<int>& nums){
         
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkDiffArray(vector<int>& nums, vector<vector<int>>& queries, int mid){
+        int n = nums.size();
+        vector<int> diff(n+1,0);
+
         for(int i = 0; i <= mid; i++){
             int l = queries[i][0];
             int r = queries[i][1];
             int val = queries[i][2];
 
-            diff[l] -= val;
-            diff[r+1] += val;
+            diff[l] += val;
+            diff[r+1] -= val;
         }
 
-        //calculate the prefixsum
-        prefixSum[0] = diff[0];
-        for(int i = 1; i < n; i++){
-            prefixSum[i] = prefixSum[i-1] + diff[i];
-        }
-
-        int zeroCount = 0;
+        //cumulative sum
+        int cumSum = 0;
         for(int i = 0; i < n; i++){
-            if(abs(prefixSum[i]) >= nums[i]){
-                zeroCount++;
+            diff[i] = cumSum + diff[i];
+            cumSum = diff[i];
+        }
+
+        //check
+        for(int i = 0; i < n; i++){
+            if(nums[i] - diff[i] > 0){
+                return false;
             }
         }
-        if(zeroCount == n){
-            result = min(result, mid);
-        }
-        return zeroCount == n;
+        return true;
     }
+
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
         int m = queries.size();
+        int l = 0, r = m-1;
+        int ans = -1;
 
-        ll sum = accumulate(nums.begin(),nums.end(),0LL);
-        if(sum == 0){
+        bool flag = checkZero(nums);
+        if(flag == true){
             return 0;
         }
 
-        int l = 0, r = m-1;
-        int result = INT_MAX;
         while(l <= r){
             int mid = l+(r-l)/2;
-            bool flag = solve(nums, queries,mid,result);
-            if(flag){
-                r = mid-1;
+
+            if(checkDiffArray(nums, queries, mid)){
+                //check for better ans 
+                r = mid -1;
+                ans = mid+1;
             }
             else{
-                l = mid+1;
+                l = mid +1;
             }
         }
 
-        return result == INT_MAX ? -1: result+1;
+        return ans;
     }
 };
