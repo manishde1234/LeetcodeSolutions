4a45061741fdc69d7f3941f128f14cfd54cb490e
manishde1234 <91026206+manishde1234@users.noreply.github.com>
2025-04-05 04:38:37 +0530

    Time: 3 ms (11.57%) | Memory: 16.5 MB (22.16%) - LeetSync

---
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   int maxD = 0;
+    map<int,int>mp;
+
+    void dep(TreeNode* root, int depth){
+        if(root == NULL){
+            return;
+        }
+        maxD = max(maxD, depth);
+        mp[root -> val] = depth;
+        dep(root -> left, depth+1);
+        dep(root -> right, depth+1);
+    }
+
+    TreeNode* solve(TreeNode* root){
+        if(root == NULL){
+            return NULL;
+        }
+        if(mp[root -> val] == maxD){
+            //last node
+            return root;
+        }
+
+        TreeNode* leftside = solve(root -> left);
+        TreeNode* rightside = solve(root -> right);
+
+        if(leftside != NULL && rightside != NULL){
+            return root;
+        }
+        if(leftside != NULL){
+            return leftside;
+        }
+        return rightside;
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+         //find the depth
+        dep(root,0);
+
+        //function with lca
+        return solve(root);
+    }
+};
