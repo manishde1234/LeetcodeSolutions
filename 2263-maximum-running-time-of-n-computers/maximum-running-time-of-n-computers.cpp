de41e40056b62674c01387bf4f8774dad62fe3b9
manishde1234 <91026206+manishde1234@users.noreply.github.com>
2025-12-02 21:44:02 +0530

    Time: 26 ms (46.93%) | Memory: 59.9 MB (29.13%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
-    long long maxRunTime(int n, vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        long long total = accumulate(arr.begin(), arr.end(), 0LL);
+    typedef long long ll;
 
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            if (arr[i] <= total / n) break;
-            total -= arr[i];
-            n--;
+    bool possible( vector<int>& batteries,int n,ll  midpoint){
+        ll totalsum = n*midpoint;
+        ll sum = 0;
+
+        for(int i = 0; i < batteries.size(); i++){
+            sum += min((ll)batteries[i],midpoint);
+            if( sum >= totalsum){
+                return true;
+            }
+        } 
+        return false;
+    }
+    long long maxRunTime(int n, vector<int>& batteries) {
+        ll result = 0;
+
+        //binary search on the ans
+        ll l = *min_element(batteries.begin(),batteries.end());
+        ll totalsum = accumulate(batteries.begin(),batteries.end(),0LL);
+
+        ll r = totalsum/n;
+
+        //l <= r cause we need to check the midpoint too
+        while(l <= r){
+            ll midpoint = l +(r-l)/2;
+            if(possible(batteries,n,midpoint) == true){
+                //find better ans
+                result = midpoint;
+                l = midpoint+1;
+            }
+            else{
+                r = midpoint-1;
+            }
         }
-
-        return total / n;
+        return result;
     }
 };
